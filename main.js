0eae0804c868ba0fec9bddf3e111357781899b37
Rick Stevens <rick.stevens@greenhousegroup.com>
2020-07-28 09:14:47 +0200

    Add script

---
+++ main.js
@@ -0,0 +1,16 @@
+import { bold, green, white, cyan } from 'https://deno.land/std/fmt/colors.ts';
+
+console.log(cyan(`
+ ┌────────────────────────────────────┐
+ │                                    │
+ │   SCRUM MASTER GENERATOR 9000 🦕   │
+ │                                    │
+ └────────────────────────────────────┘
+`));
+
+await new Promise(resolve => setTimeout(resolve, 3000));
+console.log(bold(`De ${Deno.args.length} kanshebbers voor komende sprint zijn: ${green(Deno.args.join(white(', ')))}\n`));
+await new Promise(resolve => setTimeout(resolve, 3000));
+console.log(bold('En de gelukkige nieuwe scrum master wordt...\n'));
+await new Promise(resolve => setTimeout(resolve, 4000));
+console.log(bold(`...${green(Deno.args[Math.floor(Math.random() * Deno.args.length)])}!\n`));
